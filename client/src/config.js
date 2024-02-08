e513742e2728394444b7ede25d925c2a4e5116ea
Yashasvi Yadav <124666305+yashasviyadav1@users.noreply.github.com>
2024-02-09 01:15:34 +0530

    updated base url of backend server

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,2 @@
-export const BACKEND_BASE_URL = 'http://localhost:8080';
+// export const BACKEND_BASE_URL = 'http://localhost:8080';    //(local backend server)
+export const BACKEND_BASE_URL='https://track-treasury-backend.vercel.app/' // (deployment backend server)
